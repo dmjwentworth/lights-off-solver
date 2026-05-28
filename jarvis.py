aa838826ca01c0e1ac19f3ab718235f56e117b9c
Dougal Maurice John Wentworth <dougalwentworth@gmail.com>
2026-05-28 15:05:03 +0100

    jarvis.py now setup to animate solutions - now just need to find solutions

--- jarvis.py
+++ jarvis.py
@@ -1,8 +1,8 @@
 import argparse
-import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
-from play import Game, colours
+from play import Game
+from solver import solve
 plt.style.use('dark_background')
 
 
@@ -10,18 +10,32 @@ class Jarvis(Game):
     def __init__(self):
         super().__init__(interactive=False, setup=False)
         self.fig, self.ax = plt.subplots()
-        self.ax.set_xticks([])
-        self.ax.set_yticks([])
-        self.move_history = []
+        self.moves = solve(self.grid)
+        self.anim = FuncAnimation(
+            self.fig,
+            self.animate,
+            frames=len(self.moves),
+            init_func=self.init_frame,
+            interval=700,
+            repeat=False
+        )
 
-    def show(self):
+    def init_frame(self):
+        self.ax.clear()
         self.ax.imshow(self.grid, cmap='Blues', vmin=0, vmax=1)
-        plt.show()
+
+    def animate(self, i):
+        move = self.moves[i]
+        print(f"Move {i + 1}: Toggle ({move[0]}, {move[1]})")
+        self.toggle_button(move[0], move[1])
+        self.ax.clear()
+        self.ax.imshow(self.grid, cmap='Blues', vmin=0, vmax=1)
+        plt.draw()
 
 
 def main(args):
     jarvis = Jarvis()
-    jarvis.show()
+    plt.show()
 
 
 if __name__ == '__main__':
